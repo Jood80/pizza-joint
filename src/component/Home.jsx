239ddae00adaa59f15e51d6ea8ac72ce5b9cedf8
Nujood <shawar.nujood@gmail.com>
2020-08-30 17:52:50 +0300

    add repeating motion for home page button on hover using -key frame-

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const buttonVariants = {
+  hover: {
+    // key frame
+    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1],
+    textShadow: " 0px 0px 8px rgb(255, 255, 255)",
+    boxShadow: " 0px 0px 8px rgb(255, 255, 255)",
+  },
+};
+
 const Home = () => (
   <motion.div
     className="home container"
@@ -10,15 +19,9 @@ const Home = () => (
     transition={{ delay: 1.5, duration: 1.5 }}
   >
     <h2>Welcome to Pizza Joint</h2>
+
     <Link to="/base">
-      <motion.button
-        whileHover={{
-          scale: 1.1,
-          textShadow: " 0px 0px 8px rgb(255, 255, 255)",
-          boxShadow:" 0px 0px 8px rgb(255, 255, 255)"
-        }}
-        animate={{}}
-      >
+      <motion.button variants={buttonVariants} whileHover="hover">
         Create Your Pizza
       </motion.button>
     </Link>
