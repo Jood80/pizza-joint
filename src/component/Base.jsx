6885ce59a55abdc308ce76d3533bd14c745e366d
Nujood <shawar.nujood@gmail.com>
2020-08-30 16:37:41 +0300

    add transition and hover effects

--- src/component/Base.jsx
+++ src/component/Base.jsx
@@ -1,34 +1,71 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Base = ({ addBase, pizza }) => {
   const bases = ["classic", "Thick Crust", "Thin & Crispy"];
 
   return (
-    <div className="base container">
+    <motion.div
+      className="base container"
+      initial={{ x: "100vw" }}
+      animate={{ x: 0 }}
+      transition={{ type: "spring", delay: 0.5 }}
+    >
       <h3> step 1: Choose your Base</h3>
       <ul>
         {bases.map((base) => {
           let spanClass = pizza.base === base ? "active" : "";
           return (
-            <li key={base} onClick={() => addBase(base)}>
+            <motion.li
+              key={base}
+              onClick={() => addBase(base)}
+              whileHover={{
+                scale: 1.3,
+                originX: 0,
+                color: "#f8e112",
+              }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
               <span className={spanClass}>{base}</span>
-            </li>
+            </motion.li>
           );
         })}
       </ul>
       {pizza.base && (
-        <div className="next">
+        <motion.div
+          className="next"
+          initial={{ x: "-100vw" }}
+          //0 here is px as a Default
+          animate={{ x: 0 }}
+          transition={{ type: "string", stiffness: 120 }}
+        >
           <Link to="/toppings">
             {" "}
-            <button>Next</button>{" "}
+            <motion.button
+              whileHover={{
+                scale: 1.1,
+                textShadow: " 0px 0px 8px rgb(255, 255, 255)",
+                boxShadow: " 0px 0px 8px rgb(255, 255, 255)",
+              }}
+            >
+              Next
+            </motion.button>{" "}
           </Link>
           <Link to="/">
-            <button>Back</button>{" "}
+            <motion.button
+              whileHover={{
+                scale: 1.1,
+                textShadow: " 0px 0px 8px rgb(255, 255, 255)",
+                boxShadow: " 0px 0px 8px rgb(255, 255, 255)",
+              }}
+            >
+              Back
+            </motion.button>{" "}
           </Link>
-        </div>
+        </motion.div>
       )}
-    </div>
+    </motion.div>
   );
 };
 
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,13 +5,22 @@ import { motion } from "framer-motion";
 const Home = () => (
   <motion.div
     className="home container"
-    animate={{ marginTop: 200, opacity: 0.2, rotateZ: 180 }}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ delay: 1.5, duration: 1.5 }}
   >
-    <motion.h2 animate={{ fontSize: 50, color: "#ff2994", x: 100, y: -100 }}>
-      Welcome to Pizza Joint
-    </motion.h2>
+    <h2>Welcome to Pizza Joint</h2>
     <Link to="/base">
-      <motion.button animate={{ scale: 1.5 }}>Create Your Pizza</motion.button>
+      <motion.button
+        whileHover={{
+          scale: 1.1,
+          textShadow: " 0px 0px 8px rgb(255, 255, 255)",
+          boxShadow:" 0px 0px 8px rgb(255, 255, 255)"
+        }}
+        animate={{}}
+      >
+        Create Your Pizza
+      </motion.button>
     </Link>
   </motion.div>
 );
